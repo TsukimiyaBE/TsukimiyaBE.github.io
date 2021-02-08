6431116d405ffb3e169efcbdfd9cf8f9313d24c4
boymelancholy <50545243+boymelancholy@users.noreply.github.com>
2021-02-09 00:27:35 +0900

    可読性上げた

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,3 +21,25 @@ function loadEmojis() {
     );
     return array;
 }
+
+function getWeapons(name = String, description = Array, imageName = String, last = false) {
+    let trigger = imageName;
+    var text = "";
+    text += "<div class=\"popup_wrap\">";
+    text += "<input id=\"" + trigger + "\" type=\"checkbox\">";
+    text += "<div class=\"popup_overlay\"><div class=\"popup_content\">";
+    text += "<label for=\"" + trigger + "\" class=\"close_btn\">✖</label><p>";
+    for (let i = 0; i < description.length; i++) {
+        text += description[i] ?? "";
+        if (i < description.length) {
+            text += "<br />";
+        }
+    }
+    text += "</p></div></div></div><a href=\"#\" class=\"image fit\"><div class=\"scrollanime\"><label for=\"" + trigger + "\">";
+    text += "<img src=\"../images/weapons/" + imageName + ".png\" alt=\"\" /></label></div></a>";
+    text += "<header><h3>" + name + "</h3></header>";
+    if (!last) {
+        text += "<br />"
+    }
+    return text;
+}

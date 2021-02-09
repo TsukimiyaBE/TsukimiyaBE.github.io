f314947a93e49b858158234ff0b144939750eaa6
boymelancholy <50545243+boymelancholy@users.noreply.github.com>
2021-02-09 17:20:40 +0900

    パス調整

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,21 @@
 function loadEmojis() {
     let array = [];
     let datas = new Map();
-    datas.set(':food:', '../images/emojis/food.png');
-    datas.set(':armor:', '../images/emojis/armor.png');
-    datas.set(':heart:', '../images/emojis/heart.png');
-    datas.set(':diamond:', '../images/emojis/diamond.png');
-    datas.set(':iron_ingot:', '../images/emojis/iron_ingot.png');
-    datas.set(':gold_coin:', '../images/emojis/gold_coin.png');
-    datas.set(':sunglass:', '../images/emojis/sunglass.png');
-    datas.set(':--:', '../images/emojis/expressionless.png');
-    datas.set(':o:', '../images/emojis/open_mouth.png');
-    datas.set(':tt:', '../images/emojis/tear.png');
-    datas.set(':++:', '../images/emojis/neutral_face.png');
-    datas.set(':^^:', '../images/emojis/smile.png');
-    datas.set(':#--:', '../images/emojis/rage.png');
-    datas.set(':!:', '../images/emojis/exclamation.png');
-    datas.set(':?:', '../images/emojis/question.png');
+    datas.set(':food:', 'images/emojis/food.png');
+    datas.set(':armor:', 'images/emojis/armor.png');
+    datas.set(':heart:', 'images/emojis/heart.png');
+    datas.set(':diamond:', 'images/emojis/diamond.png');
+    datas.set(':iron_ingot:', 'images/emojis/iron_ingot.png');
+    datas.set(':gold_coin:', 'images/emojis/gold_coin.png');
+    datas.set(':sunglass:', 'images/emojis/sunglass.png');
+    datas.set(':--:', 'images/emojis/expressionless.png');
+    datas.set(':o:', 'images/emojis/open_mouth.png');
+    datas.set(':tt:', 'images/emojis/tear.png');
+    datas.set(':++:', 'images/emojis/neutral_face.png');
+    datas.set(':^^:', 'images/emojis/smile.png');
+    datas.set(':#--:', 'images/emojis/rage.png');
+    datas.set(':!:', 'images/emojis/exclamation.png');
+    datas.set(':?:', 'images/emojis/question.png');
     datas.forEach( (value, key) =>
         array.push("<tr align=\"center\"><td>" + key + "</td><td><img src=\"" + value + "\" alt=\"\"/></td></tr>")
     );
@@ -36,7 +36,7 @@ function getWeapons(name = String, description = Array, imageName = String, last
         }
     }
     text += "</p></div></div></div><a href=\"#\" class=\"image fit\"><div class=\"scrollanime\"><label for=\"" + trigger + "\">";
-    text += "<img src=\"../images/weapons/" + imageName + ".png\" alt=\"\" /></label></div></a>";
+    text += "<img src=\"images/weapons/" + imageName + ".png\" alt=\"\" /></label></div></a>";
     text += "<header><h3>" + name + "</h3></header>";
     if (!last) {
         text += "<br />"
